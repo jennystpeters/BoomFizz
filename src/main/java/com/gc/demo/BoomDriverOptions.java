a9d6d10778108f1706f3574735c8699fa25f0606
jennystpeters <jennystpeters@gmail.com>
2017-07-27 13:40:59 -0400

    Test Cases Added

--- src/main/java/com/gc/demo/BoomDriverOptions.java
+++ src/main/java/com/gc/demo/BoomDriverOptions.java
@@ -1,14 +1,29 @@
+/*
 package com.gc.demo;
 
 import java.util.Scanner;
 
+*/
 /**
  * Created on 7/27/2017.
- */
+ *//*
+
 public class BoomDriver {
     public static void main(String[] args) {
+*/
+/*        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Please enter a number: ");
+        int number = 0;
+
+        if (scan.hasNextInt()) {
+            number = scan.nextInt();
+        }*//*
+
+
         BoomFizz boom = new BoomFizz();
         String val = "";
+        //for (int i = 0; i < number; i++) {
         for (int i = 0; i < 20; i++) {
             val = boom.boom(i);
             System.out.println(val);
@@ -16,3 +31,4 @@ public class BoomDriver {
 
     }
 }
+*/
